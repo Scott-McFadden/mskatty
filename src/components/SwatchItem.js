af986ed1048c394cd7f4a00b7c07589509bd8339
improvingcomputers <improvingcomputers@yahoo.com>
2020-06-16 19:11:47 -0500

    Reordered swatches with id instead of name added highlight to prem.

--- src/components/SwatchItem.js
+++ src/components/SwatchItem.js
@@ -3,16 +3,25 @@ import {  Badge } from 'react-bootstrap';
 
 class SwatchItem extends React.Component
 {
+
     render()
     {
         return (
-            <div style={{boxShadow: "5px 5px 5px 5px #888888", backgroundColor: "white", width: "250px", marginRight: "10px", marginLeft: "10px", marginBottom: "50px", padding: "10px 25px", borderRadius: "4px", border: "solid 1px black"}}>
+            <div style={{boxShadow: "5px 5px 5px 5px #888888",
+                backgroundColor: (this.props.item.cat.includes("prem") ? "#ffd9d9" : "white"),
+                width: "250px",
+                marginRight: "10px",
+                marginLeft: "10px",
+                marginBottom: "50px",
+                padding: "10px 25px",
+                borderRadius: "4px",
+                border: "solid 1px black"}}>
                 <div style={{display: "flex", justifyContent: "space-between"}}>
                     <div><strong>Swatch ID</strong></div>
                     <div><strong>Date Added</strong></div>
                 </div>
                 <div style={{display: "flex", justifyContent: "space-between"}}>
-                    <div>{this.props.item.name}</div>
+                    <div>{this.props.item.id}</div>
                     <div>{this.props.item.addDate}</div>
                 </div>
                 <div style={{
@@ -41,6 +50,7 @@ class SwatchItem extends React.Component
         for ( var a = 0; a< this.props.item.cat.length; a++ )
         {
             cat = this.props.item.cat[a];
+
             things.push(<Badge key={cat} pill variant={this.IsSelected(cat)} style={{marginLeft:"3px", marginTop: "3px"}}>{cat}</Badge> );
             things.push(" ");
         }
